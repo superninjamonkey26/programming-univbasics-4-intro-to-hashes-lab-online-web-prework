c5c27bcd2e40ba0478655e33ccdff80da3cc6e0c
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-31 21:56:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  read_from_hash = {hash => key
+  read_from_hash = {:name => "Steve"
   }
-  read_from_hash[hash]
+  read_from_hash[name]
 end
 
 def update_counting_hash(hash, key)
